bb88aeccd4f590b55688766e48048f7db5e108fe
Osamp <A00280655@mycambrian.ca>
2024-06-04 19:09:16 -0400

    Added Functions

--- shared/src/commonMain/kotlin/Calculator.kt
+++ shared/src/commonMain/kotlin/Calculator.kt
@@ -1,2 +1,21 @@
 class Calculator {
+    fun Add(left: Int, right: Int): Int {
+        var result = left + right
+        return result
+
+    }
+    fun Subtract(left: Int, right: Int): Int {
+        var result =left - right
+        return result
+    }
+
+    fun Divide(left: Int, right: Int): Int {
+        var result= left / right
+        return result
+    }
+
+    fun Multiply(left: Int, right: Int): Int {
+        var result= left * right
+        return result
+    }
 }
