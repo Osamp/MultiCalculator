9c1424d51d59211a5a6b69670ab156330a7df8da
Osamp <A00280655@mycambrian.ca>
2024-06-12 16:57:14 -0400

    Edited CalcEqualsButton Function

--- composeApp/src/androidMain/kotlin/com/jetbrains/greetings/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/greetings/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.jetbrains.greetings
 
 import App
+import Calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +26,8 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun AppAndroidPreview() {
-    App()
+fun App() {
+    CalcView()
 }
 
 @Composable
@@ -49,7 +56,40 @@ fun CalcOperationButton(){
 }
 
 @Composable
-fun CalcEqualsButton(){
+fun CalcEqualsButton(display: MutableState<String>, calculator: Calculator){
+    Button(
+        onClick = {
+            try {
+                if (display.value != "0") {
+                    val expression = display.value
+                    val parts = expression.split("\\+|\\-|\\*|\\/".toRegex())
+                    if (parts.size == 2) {
+                        val operand1 = parts[0].toInt()
+                        val operand2 = parts[1].toInt()
+                        val operation = expression.replace("\\d+".toRegex(), "").trim()
+
+                        val result = when (operation) {
+                            "+" -> calculator.Add(operand1, operand2)
+                            "-" -> calculator.Subtract(operand1, operand2)
+                            "*" -> calculator.Multiply(operand1, operand2)
+                            "/" -> calculator.Divide(operand1, operand2)
+                            else -> throw IllegalArgumentException("Unknown operator")
+                        }
+                        display.value = result.toString()
+                    } else {
+                        display.value = "Error"
+                    }
+                } else {
+                    display.value = "0"
+                }
+            } catch (e: Exception) {
+                display.value = "0"
+            }
+        },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = "=")
+    }
 
 }
 
