9e2bc24bd8d7581f14588071a838389a70507807
Osamp <A00280655@mycambrian.ca>
2024-06-12 17:01:01 -0400

    Edited  CalcOperationButton Function

--- composeApp/src/androidMain/kotlin/com/jetbrains/greetings/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/greetings/MainActivity.kt
@@ -51,8 +51,13 @@ fun CalcNumericButton(){
 }
 
 @Composable
-fun CalcOperationButton(){
-
+fun CalcOperationButton(operation: String, display: MutableState<String>){
+    Button(
+        onClick = { display.value += operation },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(operation)
+    }
 }
 
 @Composable
